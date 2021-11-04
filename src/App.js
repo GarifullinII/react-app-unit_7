4e5616ec13058821f79342cfae077caa9d5707ef
GarifullinII <ildaridrisovich@gmail.com>
2021-11-05 00:13:27 +0400

    create Add, Cart, Goods components and goods.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import goods from './goods.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+      cart: {}, // объект корзины
+      count: 0  // счетчик товаров
+    }
+  }
+
+  render() {
+    return (
+        <>
+        </>
+    )
+  }
 }
 
 export default App;
