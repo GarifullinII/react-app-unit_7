ceb1c3e4f4029abaeb6921c915ac19e5fb2049ee
GarifullinII <ildaridrisovich@gmail.com>
2021-11-05 00:37:36 +0400

    add map for goods.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import React from "react";
+import React from 'react';
 import goods from './goods.json';
+import Goods from './Goods';
 
 class App extends React.Component {
 
@@ -17,6 +18,12 @@ class App extends React.Component {
   render() {
     return (
         <>
+          <div className="container">
+            <h1>Cart</h1>
+            <div className="goods-field">
+              {goods.map(item => <Goods title={item.title} cost={item.cost} image={item.image} articul={item.articul} key={item.articul}/>)}
+            </div>
+          </div>
         </>
     )
   }
